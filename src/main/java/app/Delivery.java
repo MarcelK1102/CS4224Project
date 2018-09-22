b38b5daa378c162601d04e3fb549253cfb86fdac
Chantal <chantal.pellegrini@gmail.com>
2018-09-22 08:40:37 +0800

    get OL number

--- src/main/java/app/Delivery.java
+++ src/main/java/app/Delivery.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.sql.Date;
 
 import java.time.Instant;
+import java.util.Iterator;
 import java.util.List;
 
 import com.datastax.driver.core.ResultSet;
@@ -58,23 +59,35 @@ public class Delivery {
             //c)
             long timestamp = System.currentTimeMillis();
             System.out.println("district: " + districtNo + " :c");
-            System.out.println("time: "+ timestamp);
-            System.out.println("timestamp: "+ QueryBuilder.timestamp(timestamp).toString());
-            System.out.println("wid: "+ wid);
-            System.out.println("oloid: "+ N);
 
-            s.execute(QueryBuilder.update(Connector.keyspace, "order_line")
-            .with(QueryBuilder.set("OL_DELIVERY_D", Date.from(Instant.now())))
+            ResultSet orderlines = s.execute(QueryBuilder.select().all()
+            .from(Connector.keyspace, "order_line")
             .where(QueryBuilder.eq("OL_W_ID",wid))
             .and(QueryBuilder.eq("OL_D_ID", districtNo))
             .and(QueryBuilder.eq("OL_O_ID", N)));
 
+            Iterator<Row> it = orderlines.iterator();
+            while(it.hasNext()){
+                Row currOL = it.next();
+                int OL_Number = currOL.getInt("OL_NUMBER");
+
+                s.execute(QueryBuilder.update(Connector.keyspace, "order_line")
+                .with(QueryBuilder.set("OL_DELIVERY_D", Date.from(Instant.now())))
+                .where(QueryBuilder.eq("OL_W_ID",wid))
+                .and(QueryBuilder.eq("OL_D_ID", districtNo))
+                .and(QueryBuilder.eq("OL_O_ID", N))
+                .and(QueryBuilder.eq("OL_NUMBER", OL_Number)));
+            }
+
             //d)
             System.out.println("district: " + districtNo + " :d");
+
             int delivery_cnt = C.getInt("C_DELIVERY_CNT");
             BigDecimal c_balance = C.getDecimal("C_BALANCE");
+
             System.out.println("delivercnt:" + delivery_cnt);
             System.out.println("c_balance:" + c_balance.toString());
+
             BigDecimal B = s.execute(QueryBuilder.select()
             .sum("OL_AMOUNT")
             .from("order_line")
