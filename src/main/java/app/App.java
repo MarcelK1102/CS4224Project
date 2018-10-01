30cf4a64df78b13db7faf2480b0b2afe7efa7ffb
Marcel Kollovieh <m97.kollovieh@gmail.com>
2018-10-01 19:48:43 +0800

    commit

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,12 +2,14 @@ package app;
 
 import com.datastax.driver.core.Session;
 
+import java.math.BigDecimal;
+
 public class App {   
     static Session s = new Connector().connect();
 
     public static void main(String args[]){
         Transaction.set(s);
-        Transaction.paymentTransaction(5,5,5,10);
+        Transaction.paymentTransaction(5,5,5, BigDecimal.valueOf(10));
 
         try{
             Transaction.handleInput();
