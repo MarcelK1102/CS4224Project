74eeb60bd01d8a47658362dce8876f4bc487ae73
MorBI <morbitech1@gmail.com>
2018-10-03 10:05:06 +0800

    Fixed gradle build and logging

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -2,10 +2,13 @@ package app;
 
 import com.datastax.driver.core.Session;
 
+import org.apache.log4j.BasicConfigurator;
+
 public class App {   
     static Session s = new Connector().connect();
 
     public static void main(String args[]){
+        BasicConfigurator.configure();
         try{
             Transaction.handleInput();
             Transaction.handleOutput();
