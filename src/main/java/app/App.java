e98e22e6fe5ea51a15eca257145d300b7592820e
Marcel Kollovieh <m97.kollovieh@gmail.com>
2018-10-03 11:53:15 +0800

    o

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -17,7 +17,7 @@ public class App {
             //Transaction.paymentTransaction(5,5,5, BigDecimal.valueOf(13));
             //Transaction.getOrderStatus(5,5,1306);
             //Transaction.processDelivery(5,7);
-            Transaction.popularItem(5,5,1005);
+            Transaction.popularItem(5,5,905);
         }
         catch (Exception e ){
             System.out.println(e.toString());
