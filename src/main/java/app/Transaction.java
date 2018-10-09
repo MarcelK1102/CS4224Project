4bf110ee384d154622a42132157328ae620bf40a
MarcelK1102 <32066654+MarcelK1102@users.noreply.github.com>
2018-10-09 13:57:39 +0800

    t

--- src/main/java/app/Transaction.java
+++ src/main/java/app/Transaction.java
@@ -482,7 +482,7 @@ public class Transaction {
         for(Integer o : orders ){
             //3.a
             Row Order = Wrapper.findOrder(wid,did,o);
-            if(o!=2705)
+            if(o!=2800)
                 continue;
             System.out.println("Order ID: " + o + " Date " + Order.getTimestamp("O_ENTRY_D"));
             //3.b
