e3396da43cffe045a7ac110835e3605b4c537416
MorBI <morbitech1@gmail.com>
2018-10-09 14:27:53 +0800

    Added performance measurement code

--- db/forall.sh
+++ db/forall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+trap control_c SIGINT
 MASTER=192.168.48.249
 SEEDS=${MASTER},192.168.48.250,192.168.48.251,192.168.48.252,192.168.48.253
 CLUSTER_NAME=cs4224g
@@ -7,11 +8,11 @@ ssh ${CLUSTER_NAME}@${MASTER} mkdir -p .ssh #DO THIS IF NOT DONE
 cat ~/.ssh/id_rsa.pub | ssh ${CLUSTER_NAME}@${MASTER} 'cat >> ~/.ssh/authorized_keys'
 for i in $(echo $SEEDS | sed "s/,/ /g")
 do
-    sshpass -p "!C45zV9D" ssh $CLUSTER_NAME@$i "bash -s" < ./install_cassandra.sh $CLUSTER_NAME $SEEDS $i &
+    ssh $CLUSTER_NAME@$i "bash -s" < ./install_cassandra.sh $CLUSTER_NAME $SEEDS $i &
 done
 
 wait
 echo "DONE INSTALLING"
 
 scp *.cql ${CLUSTER_NAME}@${MASTER}:~/
-sshpass -p "!C45zV9D" ssh $CLUSTER_NAME@$MASTER "bash -s" < ./initdb.sh
+ssh $CLUSTER_NAME@$MASTER "bash -s" < ./initdb.sh
