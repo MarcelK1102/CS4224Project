0d4f5dcb31e074870ed58e866fec9922314c50e5
Chantal <chantal.pellegrini@gmail.com>
2018-10-27 12:48:20 +0800

    import

--- script/import.sh
+++ script/import.sh
@@ -1,3 +1,10 @@
+db.warehouse.drop()
+db.district.drop()
+db.customer.drop()
+db.order.drop()
+db.item.drop()
+db.order.drop()
+db.stock.drop()
 mongoimport --host 192.168.48.249 -d warehouse -c warehouse --type csv --fields W_ID,W_NAME,W_STREET_1,W_STREET_2,W_CITY,W_STATE,W_ZIP,W_TAX,W_YTD --file /temp/4224-project-files/data-files/warehouse.csv
 mongoimport --host 192.168.48.249 -d warehouse -c district --type csv --fields D_W_ID,D_ID,D_NAME,D_STREET_1,D_STREET_2,D_CITY,D_STATE,D_ZIP,D_TAX,D_YTD,D_NEXT_O_ID --file /temp/4224-project-files/data-files/district.csv
 mongoimport --host 192.168.48.249 -d warehouse -c customer --type csv --fields C_W_ID,C_D_ID,C_ID,C_FIRST,C_MIDDLE,C_LAST,C_STREET_1,C_STREET_2,C_CITY,C_STATE,C_ZIP,C_PHONE,C_SINCE,C_CREDIT,C_CREDIT_LIM,C_DISCOUNT,C_BALANCE,C_YTD_PAYMENT,C_PAYMENT_CNT,C_DELIVERY_CNT,C_DATA --file /temp/4224-project-files/data-files/customer.csv
