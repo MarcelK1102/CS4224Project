4cd4a0eef324689b311f9f584f3c3c0986bab0cc
MorBI <morbitech1@gmail.com>
2018-11-05 23:21:57 +0800

    Update install script and removed some dummy files

--- script/forall.sh
+++ script/forall.sh
@@ -1,70 +1,77 @@
 #!/bin/bash
-MASTER=192.168.48.253
-SHARDS=192.168.48.249,192.168.48.250,192.168.48.251,192.168.48.252,192.168.48.253
-CONFIGS=192.168.48.251,192.168.48.252,192.168.48.253
+#Here the three first shards will be config servers
+SHARDS=(192.168.48.249 192.168.48.250 192.168.48.251 192.168.48.252 192.168.48.253)
 CLUSTER_NAME=cs4224g
 PROJECT_PATH=..
 
-#Add ssh key for shared drive as to avoid giving password in the future
+##Add ssh key for shared drive as to avoid giving password in the future
 # ssh-keygen -t rsa
 # ssh ${CLUSTER_NAME}@${MASTER} mkdir -p .ssh #DO THIS IF NOT DONE
 # cat ~/.ssh/id_rsa.pub | ssh ${CLUSTER_NAME}@${MASTER} 'cat >> ~/.ssh/authorized_keys'
 
 #Execute install script
-for i in $(echo $SHARDS | sed "s/,/ /g")
+for i in {0..4}
 do
-    ssh $CLUSTER_NAME@$i "bash -s" < ./install.sh $i &
+    ssh $CLUSTER_NAME@${SHARDS[i]} "bash -s" < $PROJECT_PATH/script/install.sh $i &
 done
 
 wait
 echo "DONE INSTALLING"
 
-for i in $(echo $CONFIGS | sed "s/,/ /g")
+for i in {0..2}
 do
-    ssh $CLUSTER_NAME@$i "/temp/g/mongoDB/bin/mongod --configsvr --dbpath /temp/g/data/conf --bind_ip $i --replSet conf &" & #Run on 3 server
+    ssh $CLUSTER_NAME@${SHARDS[i]}  "/temp/g/mongoDB/bin/mongod --configsvr --dbpath /temp/g/data/conf --bind_ip ${SHARDS[i]} --replSet conf &" &
 done
 
-for i in $(echo $SHARDS | sed "s/,/ /g")
-do
-    ssh $CLUSTER_NAME@$i "/temp/g/mongoDB/bin/mongod --shardsvr --replSet warehouse --dbpath /temp/g/data/db/ --bind_ip $i &" &
-done
+sleep 20
 
-mongo 192.168.48.253:27019 --eval 'rs.initiate(
-  {
-    _id: "conf",
+echo "rs.initiate({
+    _id: 'conf',
     configsvr: true,
     members: [
-      { _id : 0, host : "192.168.48.251:27019" },
-      { _id : 1, host : "192.168.48.252:27019" },
-      { _id : 2, host : "192.168.48.253:27019" }
+      { _id : 0, host : '${SHARDS[0]}:27019' },
+      { _id : 1, host : '${SHARDS[1]}:27019' },
+      { _id : 2, host : '${SHARDS[2]}:27019' }
     ]
   }
-)'
+)" | ssh $CLUSTER_NAME@${SHARDS[0]} "/temp/g/mongoDB/bin/mongo ${SHARDS[0]}:27019"
 
-mongo 192.168.48.253:27018 --eval 'rs.initiate(
-  {
-    _id : "warehouse",
-    members: [
-      { _id : 0, host : "192.168.48.249:27018" },
-      { _id : 1, host : "192.168.48.250:27018" },
-      { _id : 2, host : "192.168.48.251:27018" },
-      { _id : 3, host : "192.168.48.252:27018" },
-      { _id : 4, host : "192.168.48.253:27018" }
-    ]
-  }
-)'
-
-for i in $(echo $SHARDS | sed "s/,/ /g")
+for i in {0..4}
 do
-    ssh $CLUSTER_NAME@$i "mongos --configdb conf/192.168.48.251:27019,192.168.48.252:27019,192.168.48.253:27019 --bind_ip $i &" &
+    ssh $CLUSTER_NAME@${SHARDS[i]} "/temp/g/mongoDB/bin/mongod --shardsvr --replSet warehouse --dbpath /temp/g/data/db/ --bind_ip ${SHARDS[i]} &" &
 done
 
-mongo 192.168.48.253:27017 --eval 'sh.addShard("warehouse/192.168.48.249:27018")'
-mongo 192.168.48.253:27017 --eval 'sh.addShard("warehouse/192.168.48.250:27018")'
-mongo 192.168.48.253:27017 --eval 'sh.addShard("warehouse/192.168.48.251:27018")'
-mongo 192.168.48.253:27017 --eval 'sh.addShard("warehouse/192.168.48.252:27018")'
-mongo 192.168.48.253:27017 --eval 'sh.addShard("warehouse/192.168.48.253:27018")'
+sleep 20
 
-mongo 192.168.48.253:27017 --eval "use warehouse"
-mongo 192.168.48.253:27017 --eval sh.enableSharding("warehouse")
+echo "rs.initiate({
+    _id : 'warehouse',
+    members: [
+      { _id : 0, host : '${SHARDS[0]}:27018' },
+      { _id : 1, host : '${SHARDS[1]}:27018' },
+      { _id : 2, host : '${SHARDS[2]}:27018' },
+      { _id : 3, host : '${SHARDS[3]}:27018' },
+      { _id : 4, host : '${SHARDS[4]}:27018' }
+    ]
+  }
+)" | ssh $CLUSTER_NAME@${SHARDS[0]}  "/temp/g/mongoDB/bin/mongo ${SHARDS[0]}:27018"
 
+for i in {0..4}
+do
+  ssh $CLUSTER_NAME@${SHARDS[i]} "/temp/g/mongoDB/bin/mongos --configdb conf/${SHARDS[0]}:27019,${SHARDS[1]}:27019,${SHARDS[2]}:27019 --bind_ip ${SHARDS[i]} &" &
+done
+
+sleep 20
+
+echo "
+sh.addShard('warehouse/${SHARDS[0]}:27018')
+sh.addShard('warehouse/${SHARDS[1]}:27018')
+sh.addShard('warehouse/${SHARDS[2]}:27018')
+sh.addShard('warehouse/${SHARDS[3]}:27018')
+sh.addShard('warehouse/${SHARDS[4]}:27018')
+sh.enableSharding('warehouse')
+" | ssh $CLUSTER_NAME@${SHARDS[0]} "/temp/g/mongoDB/bin/mongo ${SHARDS[0]}"
+
+ssh $CLUSTER_NAME@${SHARDS[0]} "wget https://www.comp.nus.edu.sg/~cs4224/4224-project-files.zip;unzip 4224-project-files.zip;sed -i -e 's/,null,/,,/g' 4224-project-files/data-files/*.csv"
+
+ssh $CLUSTER_NAME@${SHARDS[0]} "bash -s" < ./import.sh ${SHARDS[0]} 
+ssh $CLUSTER_NAME@${SHARDS[0]} "bash -s" < ./dbinit.sh ${SHARDS[0]} 
--- script/install.sh
+++ script/install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 THIS_IP=$1
+rm -rf /temp/g
 mkdir /temp/g
 cd /temp/g
 ps aux | grep mongo | awk '{print $2}' | xargs kill -9
-rm -rf *
-wget -O mongoDB.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-4.0.3.tgz
+wget -O mongoDB.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-4.1.4.tgz
 mkdir mongoDB && tar zxvf mongoDB.tgz -C mongoDB --strip-components 1
 mkdir data
 mkdir data/db
 mkdir data/conf
 sed -i '/mongoDB/d' ~/.profile
 echo "export PATH=\$PATH:/temp/g/mongoDB/bin" >> ~/.profile
-cd mongoDB/bin
